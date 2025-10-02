c30325f904d6900ebb878192ada6149035d5b631
Vasileios Polydoras <polydoras@wega.iam.rwth-aachen.de>
2025-10-02 08:08:40 +0200

    Ran forward relobralo and inverse with own scheme and relobralo

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from Utils import Utils
 def run_model(epochs, scheme, conditioning, inverse, filename):
     utils = Utils(epochs=epochs, scheme=scheme, conditioning=conditioning, inverse=inverse)
     utils.train()
-    utils.plot_save_results(path='Outputs/pinn_results30.09', filename=filename)
+    utils.plot_save_results(path='Outputs/pinn_results01.10', filename=filename)
 
 # create an init function that is only run as run from this file
 if __name__ == "__main__":
     
     epochs = 300001
 
-    filename = 'inverse_no_conditioning.svg'
-    run_model(epochs=epochs, scheme='own', conditioning=False, inverse=True, filename=filename)
+    # filename = 'inverse_no_conditioning.svg'
+    # run_model(epochs=epochs, scheme='own', conditioning=False, inverse=True, filename=filename)
+
+    filename = 'forward_relobralo.svg'
+    run_model(epochs=epochs, scheme='relobralo', conditioning=True, inverse=False, filename=filename)
 
     filename = 'inverse_conditioning.svg'
     run_model(epochs=epochs, scheme='own', conditioning=True, inverse=True, filename=filename)
 
-    filename = 'inverse_no_scheme.svg'
-    run_model(epochs=epochs, scheme=None, conditioning=False, inverse=True, filename=filename)
+    # filename = 'inverse_no_scheme.svg'
+    # run_model(epochs=epochs, scheme=None, conditioning=False, inverse=True, filename=filename)
 
     filename = 'inverse_relobralo.svg'
     run_model(epochs=epochs, scheme='relobralo', conditioning=False, inverse=True, filename=filename)
